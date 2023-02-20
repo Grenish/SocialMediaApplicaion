77ede659219e0279adb45c3b55866a7cb8ea9cda
Grenish rai <ariestheracer@gmail.com>
2023-02-20 15:41:37 +0530

    Login and registration page complete

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ import userRoutes from './routes/users.js';
 import postRoutes from './routes/posts.js';
 import { createPost } from './controllers/posts.js'
 import { verifyToken } from './middleware/auth.js';
+import User from './models/User.js';
+import Post from './models/Post.js';
+import { users, posts } from './data/index.js';
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -51,10 +54,18 @@ app.use('/users', userRoutes);
 app.use('/posts', postRoutes);
 
 /* MONGOOSE SETUP */
+mongoose.set('strictQuery', true);
+
+
 const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+
 }).catch((error) => console.log(`${error} did not connect`));
